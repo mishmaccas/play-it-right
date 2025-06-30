75926d58d7e731dc689014191994a0a790d1030e
Mish <mishmaccas@gmail.com>
2025-06-30 13:55:56 +1000

    update: to main

--- e2e/redirects301.spec.ts
+++ e2e/redirects301.spec.ts
@@ -6,7 +6,7 @@ import { parse } from "csv-parse/sync";
 //
 // List your CSV files here
 //
-const csvFiles = ["redirects1.csv", "redirects2.csv"]; //For TESTING
+const csvFiles = ["file1.csv", "file2.csv"];
 
 csvFiles.forEach((fileName) => {
   const csvFilePath = path.resolve("./test-data/", fileName);
@@ -20,9 +20,7 @@ csvFiles.forEach((fileName) => {
   // “I know process.env.USERNAME is not undefined or null, so don’t complain about it.”
 
   records.forEach(({ From, To }, index) => {
-    test(`${fileName} - ${
-      index + 1
-    }: redirect from ${From} to ${To} with 301`, async ({ browser }) => {
+    test(`${fileName} - ${index + 1}: redirect from ${From} to ${To} with 301`, async ({ browser }) => {
       const context = await browser.newContext({
         httpCredentials: {
           username: process.env.USERNAME!,
@@ -43,7 +41,12 @@ csvFiles.forEach((fileName) => {
       const finalUrl = page.url();
 
       // Expected redirect URL and Status code = 301
-      expect(finalUrl).toBe(To);
+      // for asian characters
+      //https://www.site.co.jp/stores/jp/北海道/札幌市/8793.html
+      //   → human-readable form (unencoded).
+      // https://www.site.co.jp/stores/jp/%E5%8C%97%E6%B5%B7%E9%81%93/%E6%9C%AD%E5%B9%8C%E5%B8%82/8793.html
+      //  → percent-encoded form, what browsers and most automation tools actually use internally.
+      expect(decodeURIComponent(finalUrl)).toBe(To);
       expect(statusCode).toBe(301);
     });
   });
